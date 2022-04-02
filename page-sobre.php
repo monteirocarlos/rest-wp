7bb4345c25aa1a98c9992c7d9ffeb6f64117a85e
Carlos <carlosteste360@gmail.com>
2022-04-02 16:55:21 -0300

    Implementando o ACF na pagina sobre

--- page-sobre.php
+++ page-sobre.php
@@ -5,18 +5,18 @@
 			<h2 class="subtitulo"><?php the_title(); ?></h2>
 
 			<div class="grid-8">
-				<img src="img/rest-fachada.jpg" alt="Fachada do Rest">
+				<img src="<?php the_field('imagem_sobre');?>" alt="<?php the_field('descricao_imagem_sobre');?>">
 			</div>
 
 			<div class="grid-8">
-				<h2>História</h2>
-				<p>O incentivo ao avanço tecnológico, assim como a mobilidade dos capitais internacionais auxilia a preparação e a composição dos níveis de motivação departamental.</p>
-				<p>Gostaria de enfatizar que o desenvolvimento contínuo de distintas formas de atuação prepara-nos para enfrentar situações atípicas decorrentes do remanejamento dos quadros funcionais.</p>
-				<h2>Visão</h2>
-				<p>Não obstante, a expansão dos mercados mundiais faz parte de um processo de gerenciamento de alternativas às soluções ortodoxas.</p>
-				<h2>Valores</h2>
-				<p>O empenho em analisar a consolidação das estruturas apresenta tendências no sentido de aprovar a manutenção dos índices pretendidos.</p>
+				<h2><?php the_field('titulo_historia');?></h2>
+				<?php the_field('historia-info');?>
+				<h2><?php the_field('titulo_visao');?></h2>
+				<?php the_field('visao-info');?>
+				<h2><?php the_field('titulo_valores');?></h2>
+				<?php the_field('valores-info');?>
 			</div>
+			
 		</section>
 
 <?php endwhile; else : endif; ?>
