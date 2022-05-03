cfb7c3aa4b48fee04072d0ff773693007ebe8e71
Carlos <carlosteste360@gmail.com>
2022-05-03 20:47:15 -0300

    Inserido Header e Footer gerenciaveis

--- header.php
+++ header.php
@@ -1,32 +1,31 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="utf-8" />
-    <title>Rest - <?php the_title(); ?></title>
+	<head>
+		<meta charset="utf-8">
+		<title><?php bloginfo('name'); ?></title>
 
-    <link
-      href="https://fonts.googleapis.com/css?family=Alegreya+SC"
-      rel="stylesheet"
-      type="text/css"
-    />
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/style.css" />
-    <!-- Header WordPress -->
-    <?php wp_head(); ?>
-    <!-- Fecha Header WordPress -->
-  </head>
+		<link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
 
-  <body>
-    <header>
-      <nav>
-        <ul>
-          <li class="current_page_item"><a href="/">Menu</a></li>
-          <li><a href="/wordpress/sobre">Sobre</a></li>
-          <li><a href="/wordpress/contato">Contato</a></li>
-        </ul>
-      </nav>
+		<!-- Header Wordpress -->
+		<?php wp_head(); ?>
+		<!-- Fecha Header Wordpress -->
+	</head>
+	<?php $_sobre = get_page_by_title('sobre'); ?>
+	<body>
+		
+		<header>
+			<nav>
+				<ul>
+					<li class="current_page_item"><a href="http://wordpress.local">Menu</a></li>
+					<li><a href="/testewp/sobre/">Sobre</a></li>
+					<li><a href="/testewp/contato/">Contato</a></li>
+				</ul>
+			</nav>
 
-      <h1><img src="<?php echo get_stylesheet_directory_uri()?>/img/rest.png" alt="Rest" /></h1>
+			<h1><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/rest.png" alt="Rest"></h1>
 
-      <p>Rua Marechal 29 – Copacabana – Rj</p>
-      <p class="telefone">2422-9201</p>
-    </header>
+			<?php $contato = get_page_by_title('contato'); ?>
+			<p><?php the_field('endereco_header', $contato); ?></p>
+			<p class="telefone"><?php the_field('telefone_header', $contato); ?></p>
+		</header>
